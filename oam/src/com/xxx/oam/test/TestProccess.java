c2aa68101b7a1f9997a2aa070dea18f694a3377e
default <default@DESKTOP-7FEGRP2>
2018-07-31 17:55:22 +0800

    update

--- oam/src/com/xxx/oam/test/TestProccess.java
+++ oam/src/com/xxx/oam/test/TestProccess.java
@@ -7,22 +7,23 @@ public class TestProccess {
 
 	public static void main(String[] args) {
 		String result = "";
- 
+		int count=0;
         try {
             Process process = Runtime.getRuntime().exec("python E:/pythonworkspace/test_python/testTime.py" );
             
             InputStreamReader ir = new InputStreamReader(process.getInputStream(),"GBK");
             LineNumberReader input = new LineNumberReader(ir);
-            result=input.readLine();
+            while((result=input.readLine())!=null ) {
+            	/*result+=input.readLine();
+            	count++;*/
+            	System.out.println(result);
+            }
             input.close();
             ir.close();
 //            process.waitFor();
         } catch (Exception e) {
             System.out.println("调用python脚本并读取结果时出错：" + e.getMessage());
         }
-        
         System.out.println(result);
- 
 	}
-
 }
