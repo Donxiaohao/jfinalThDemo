ea139b33ca37eb31474fe33e467cbfc28bd50520
default <default@DESKTOP-7FEGRP2>
2018-10-11 16:50:53 +0800

    update

--- oam/src/com/xxx/oam/test/TestTX.java
+++ oam/src/com/xxx/oam/test/TestTX.java
@@ -1,7 +1,10 @@
 package com.xxx.oam.test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.jetty.util.ajax.JSON;
 import org.junit.Test;
 
 import com.jfinal.aop.Before;
@@ -11,6 +14,7 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.IAtom;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.SqlPara;
 import com.jfinal.plugin.activerecord.tx.Tx;
 import com.jfinal.plugin.druid.DruidPlugin;
 public class TestTX {
@@ -24,18 +28,30 @@ public class TestTX {
 		dp.start();
 		arp.start();
 		
-		Db.tx(new IAtom() {
+		/*Db.tx(new IAtom() {
 			@Override
 			public boolean run() throws SQLException {
 				for(int i=0;i<10;i++) {
-					/*if(i==8) {
+					if(i==8) {
 						throw new RuntimeException("测试");
-					}*/
+					}
 					Record set = new Record().set("name","testTx");
 					Db.save("test_in", set);
 				}
 				return true;
 			}
-		});
+		});*/
+		List<Record> records=new ArrayList<Record>();
+		Record record=new Record();
+		record.set("id", 8);
+		Record record2=new Record();
+		record2.set("id", 3);
+		records.add(record);
+		//records.add(record2);
+		SqlPara sqlPara =new SqlPara();
+		sqlPara.setSql("select * from test2 where id = ? ");
+		sqlPara.addPara(records);
+		List<Record> find = Db.find(sqlPara);
+		System.out.println(JSON.toString(find));
 	}
 }
